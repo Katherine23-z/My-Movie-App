99f9ffe3056271bd652495b4d780cb22d3fcebc4
Katherine23-z <eszaharova23@yandex.ru>
2021-07-20 15:52:55 +0300

    Lesson10

--- app/src/main/java/com/example/movieapplication/framework/ui/main/fragments/MainFragment.kt
+++ app/src/main/java/com/example/movieapplication/framework/ui/main/fragments/MainFragment.kt
@@ -1,6 +1,14 @@
 package com.example.movieapplication.framework.ui.main.fragments
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.*
 import android.widget.LinearLayout
@@ -25,10 +33,16 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.example.movieapplication.framework.ui.history_fragment.HistoryFragment
 import com.example.movieapplication.framework.ui.main.*
+import kotlinx.android.synthetic.main.main_fragment.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.IOException
 
-class MainFragment : Fragment(), CoroutineScope by MainScope(){
+class MainFragment : Fragment(), CoroutineScope by MainScope() {
     companion object {
         fun newInstance() = MainFragment()
     }
@@ -36,9 +50,24 @@ class MainFragment : Fragment(), CoroutineScope by MainScope(){
 
     private val viewModel: MainViewModel by lazy { ViewModelProvider(this).get(MainViewModel::class.java) }
     private lateinit var navigation: Navigation
-    private lateinit var mainView : LinearLayout
-    private var _binding : MainFragmentBinding? = null
+    private lateinit var mainView: LinearLayout
+    private var _binding: MainFragmentBinding? = null
     private val binding get() = _binding!!
+    private val permissionResult = registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+                if(result) {
+                    getLocation()
+                } else {
+                    Toast.makeText(context, getString(R.string.need_permission_to_find_location), Toast.LENGTH_SHORT).show()
+                }
+    }
+
+    private val onLocationListener = object : LocationListener{
+        override fun onLocationChanged(location: Location) {
+            getAddressAsync(location)
+        }
+
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,20 +78,21 @@ class MainFragment : Fragment(), CoroutineScope by MainScope(){
                               savedInstanceState: Bundle?): View {
         _binding = MainFragmentBinding.inflate(inflater, container, false)
         val view = binding.root
-        val activity : MainActivity = context as MainActivity
+        val activity: MainActivity = context as MainActivity
         navigation = activity.getNavigation()
+        binding.geoLocation.setOnClickListener { checkPermission() }
         val isAdult = activity.getSharedPreferences(MY_SETTINGS, Context.MODE_PRIVATE).getBoolean(IS_ADULT, true)
-        if(isAdult){
+        if (isAdult) {
             initRecyclers()
             initHeaders()
-        }else {
+        } else {
             Toast.makeText(context, getString(R.string.Adult), Toast.LENGTH_SHORT).show()
         }
         mainView = binding.mainView
         return view
     }
 
-    fun initHeaders(){
+    fun initHeaders() {
         val horrorHeader: TextView = binding.headerHorrors
         horrorHeader.setOnClickListener {
             navigation.addFragment(GenreFragment.newInstance(), true)
@@ -129,7 +159,7 @@ class MainFragment : Fragment(), CoroutineScope by MainScope(){
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel.getLiveData().observe(viewLifecycleOwner, Observer{renderData(it as AppState)})
+        viewModel.getLiveData().observe(viewLifecycleOwner, Observer { renderData(it as AppState) })
         viewModel.getDataFromLocalSourceHorrors()
     }
 
@@ -146,7 +176,7 @@ class MainFragment : Fragment(), CoroutineScope by MainScope(){
             }
             is AppState.Error -> {
                 binding.mainFragmentLoadingLayout.visibility = View.GONE
-                mainView.showSnackBar(getString(R.string.error), getString(R.string.reload), {viewModel.getDataFromLocalSourceHorrors()})
+                mainView.showSnackBar(getString(R.string.error), getString(R.string.reload), { viewModel.getDataFromLocalSourceHorrors() })
             }
         }
 
@@ -170,13 +200,86 @@ class MainFragment : Fragment(), CoroutineScope by MainScope(){
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        when (item.itemId) {
             R.id.action_settings -> navigation.addFragment(SettingsFragment.newInstance(), true)
             R.id.action_add_to_fav -> navigation.addFragment(HistoryFragment.newInstance(), true)
             R.id.action_content_provider -> navigation.addFragment(ContentProviderFragment.newInstance(), true)
         }
         return super.onOptionsItemSelected(item)
     }
+
+    private fun checkPermission() {
+        activity?.let {
+            when {
+                ContextCompat.checkSelfPermission(it, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED -> {
+                    getLocation()
+                }
+
+                else -> {
+                    permissionResult.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation() {
+        val locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            val provider = locationManager.getProvider(LocationManager.GPS_PROVIDER)
+            provider.let {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000L, 10f, onLocationListener)
+            }
+        } else {
+            val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if(location == null){
+                Toast.makeText(requireContext(), getString(R.string.looks_like_location_disabled), Toast.LENGTH_SHORT).show()
+            } else {
+                getAddressAsync(location)
+            }
+        }
+    }
+
+    private fun getAddressAsync(location: Location){
+        val geoCoder = Geocoder(context)
+        launch(Dispatchers.IO) {
+            try {
+                val addresses = geoCoder.getFromLocation(location.latitude, location.longitude, 1)
+                geoLocation.post { showAddressDialog(addresses[0].getAddressLine(0), location) }
+            }catch (e:IOException){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun showAddressDialog(address: String, location: Location){
+        activity?.let {
+            AlertDialog.Builder(it).setTitle("Ваш адрес").setMessage(address).setPositiveButton(""){_, _ ->
+                navigation.addFragment(SettingsFragment.newInstance(), true)
+            }.setNegativeButton("Закрыть") {dialog, _ -> dialog.dismiss() }.create().show()
+        }
+    }
+
+
+
+
+    /*private fun showRationaleDialog() {
+        activity?.let {
+            AlertDialog.Builder(it)
+                    .setTitle("")
+                    .setMessage("")
+                    .setPositiveButton("") { _, _ ->
+                        requestPermission()
+                    }
+                    .setNegativeButton("") { dialog, _ -> dialog.dismiss() }
+                    .create()
+                    .show()
+        }
+    }*/
+
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
